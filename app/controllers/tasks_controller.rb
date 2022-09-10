6ad133b8b8424b9e3d5ea950e58ba035e754a8d2
duvan <duvanfoz@hotmail.com>
2022-09-10 14:32:44 -0500

    feat: views with bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task= Task.new(task_params).save
+    @task = Task.new(task_params).save
     redirect_to tasks_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :posts
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new_task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :task
-  get 'tasks/:id', to: 'tasks#show', as: :show
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#delete'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
